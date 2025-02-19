318fe4badcdff84b78410e97d2c539a4349ded75
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-19 18:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.querySelectorAll(".square").forEach(square => {
+    square.addEventListener("mouseenter", function () {
+        document.querySelectorAll(".square").forEach(sq => {
+            if (sq !== this) {
+                sq.style.backgroundColor = "#6F4E37"; 
+            }
+        });
+    });
+
+    square.addEventListener("mouseleave", function () {
+        document.querySelectorAll(".square").forEach(sq => {
+            sq.style.backgroundColor = "#E6E6FA"; 
+        });
+    });
+});
